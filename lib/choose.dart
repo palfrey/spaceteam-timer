0df3fb81294ab447824caa317ec69c1345872b09
Tom Parker-Shemilt <palfrey@tevp.net>
2018-12-24 12:01:43 +0000

    Resize display on select difficulty screen

--- lib/choose.dart
+++ lib/choose.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'difficulty.dart';
 import 'timer.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class TimerSelect extends StatelessWidget {
   void selectDifficulty(BuildContext context, Difficulty level) {
@@ -18,28 +19,36 @@ class TimerSelect extends StatelessWidget {
       appBar: AppBar(
         title: Text('Spaceteam Timer'),
       ),
-      body: Center(
-          child: Column(children: <Widget>[
-        Text(
-          'Select difficulty',
-          style: TextStyle(fontSize: 24.0),
-        ),
-        RaisedButton(
-            child: Text('Easy (7 minutes)'),
-            onPressed: () {
-              selectDifficulty(context, Difficulty.Easy);
-            }),
-        RaisedButton(
-            child: Text('Medium (5 minutes)'),
-            onPressed: () {
-              selectDifficulty(context, Difficulty.Medium);
-            }),
-        RaisedButton(
-            child: Text('Hard (3 minutes)'),
-            onPressed: () {
-              selectDifficulty(context, Difficulty.Hard);
-            })
-      ])),
+      body: Row(children: <Widget>[
+        Expanded(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              AutoSizeText(
+                'Select difficulty',
+                maxLines: 1,
+                style: TextStyle(fontSize: 120.0),
+              ),
+              RaisedButton(
+                  child: AutoSizeText('Easy (7 minutes)',
+                      maxLines: 1, style: TextStyle(fontSize: 120.0)),
+                  onPressed: () {
+                    selectDifficulty(context, Difficulty.Easy);
+                  }),
+              RaisedButton(
+                  child: AutoSizeText('Medium (5 minutes)',
+                      maxLines: 1, style: TextStyle(fontSize: 120.0)),
+                  onPressed: () {
+                    selectDifficulty(context, Difficulty.Medium);
+                  }),
+              RaisedButton(
+                  child: AutoSizeText('Hard (3 minutes)',
+                      maxLines: 1, style: TextStyle(fontSize: 120.0)),
+                  onPressed: () {
+                    selectDifficulty(context, Difficulty.Hard);
+                  })
+            ]))
+      ]),
     );
   }
 }
